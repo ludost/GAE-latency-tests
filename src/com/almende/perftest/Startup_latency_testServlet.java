82236b2ace3aaeb8c2e90dab2ae3667f165a139d
Ludo Stellingwerff <ludo@almende.org>
2012-04-24 09:28:16 +0200

    Introducing some more jars: json-lib & UUID

--- src/com/almende/perftest/Startup_latency_testServlet.java
+++ src/com/almende/perftest/Startup_latency_testServlet.java
@@ -3,11 +3,20 @@ package com.almende.perftest;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+import net.sf.json.JSONSerializer;
+
+import com.eaio.uuid.UUID;
+
 @SuppressWarnings("serial")
 public class Startup_latency_testServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		
+		UUID exampleUUID = new UUID();
+		resp.getWriter().println("Generated UUID:"+exampleUUID);
+		
+		resp.getWriter().println("As JSON, running json-lib: "+JSONSerializer.toJSON(exampleUUID));
 	}
 }
